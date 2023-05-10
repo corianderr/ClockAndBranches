782a80206bb4af963dfc15c9635921ba2868c1fc
cariander <carianderrr@gmail.com>
2023-05-10 19:11:30 +0600

    Implement angle calculation logic and output for user

--- Clock/Program.cs
+++ Clock/Program.cs
@@ -1,9 +1,19 @@
-﻿Console.WriteLine("Hello! Welcome to Angle Counting app!\nYou give us the hours and minutes, " +
-              "and we in turn give you the smaller angle between the hands of analogue clock! Pretty cool, isn't it?");
+﻿const int oneMinuteDegreeForMinutesArrow = 6;
+const double oneMinuteDegreeForHoursArrow = 0.5;
+
+Console.WriteLine("Hello! Welcome to Angle Counting app!\nYou give us the hours and minutes, " +
+                  "and we in turn give you the smaller angle between the hands of analogue clock! Pretty cool, isn't it?\n");
+
 var hour = GetIntFromInputInRange("hour",1, 12);
-var minutes = GetIntFromInputInRange("minutes", 1, 60);
+var minutes = GetIntFromInputInRange("minutes", 1, 59);
 
+var hourAngle = (hour * 60 + minutes) * oneMinuteDegreeForHoursArrow;
+var minutesAngle = minutes * oneMinuteDegreeForMinutesArrow;
 
+var minAngle = Math.Abs(hourAngle - minutesAngle);
+minAngle = minAngle <= 180 ? minAngle : 360 - minAngle;
+
+Console.WriteLine($"The lesser angle between hours and minutes arrows is {minAngle}");
 
 
 int GetIntFromInputInRange(string name, int min, int max)
