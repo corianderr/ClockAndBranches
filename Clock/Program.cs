6abab22356ae227efa859b9dcfb6f5a9027c5544
cariander <carianderrr@gmail.com>
2023-05-10 19:17:39 +0600

    Beautify output and code

--- Clock/Program.cs
+++ Clock/Program.cs
@@ -1,19 +1,23 @@
 ﻿const int oneMinuteDegreeForMinutesArrow = 6;
 const double oneMinuteDegreeForHoursArrow = 0.5;
 
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Hello! Welcome to Angle Counting app!\nYou give us the hours and minutes, " +
                   "and we in turn give you the smaller angle between the hands of analogue clock! Pretty cool, isn't it?\n");
+Console.ResetColor();
 
-var hour = GetIntFromInputInRange("hour",1, 12);
+var hours = GetIntFromInputInRange("hour", 1, 12);
 var minutes = GetIntFromInputInRange("minutes", 1, 59);
 
-var hourAngle = (hour * 60 + minutes) * oneMinuteDegreeForHoursArrow;
+var hourAngle = (hours * 60 + minutes) * oneMinuteDegreeForHoursArrow;
 var minutesAngle = minutes * oneMinuteDegreeForMinutesArrow;
 
 var minAngle = Math.Abs(hourAngle - minutesAngle);
 minAngle = minAngle <= 180 ? minAngle : 360 - minAngle;
 
-Console.WriteLine($"The lesser angle between hours and minutes arrows is {minAngle}");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"\nThe lesser angle between the arrows indicating {hours} hours and {minutes} minutes is {minAngle} degrees!");
+Console.ResetColor();
 
 
 int GetIntFromInputInRange(string name, int min, int max)
@@ -23,7 +27,10 @@ int GetIntFromInputInRange(string name, int min, int max)
         $"Enter {name}: ");
     while (!(Int32.TryParse(Console.ReadLine(), out num) && num <= max && num >= min))
     {
-        Console.Write($"Be careful, input must be must be an integer number between {min} and {max} inclusive. Try again: ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(
+            $"Be careful, input must be must be an integer number between {min} and {max} inclusive.\nTry again: ");
+        Console.ResetColor();
     }
 
     return num;
